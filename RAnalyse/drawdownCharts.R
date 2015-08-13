2596abbd9c3759f6d57cc00fe197f100c018462a
lotgon <andrei1pazniak@gmail.com>
2015-08-13 16:02:34 +0300

    add figure

--- RAnalyse/drawdownCharts.R
+++ RAnalyse/drawdownCharts.R
@@ -4,3 +4,4 @@ d<-fread(fileName)
 require(ggplot2)
 ggplot(d[Drawdown>250*10^-5 & Drawdown<5000*10^-5], aes(x=Drawdown*100000)) + facet_grid(Tp ~ .) + geom_density()
 
+ggplot(d, aes(x=Drawdown, y=BarDuration)) + geom_point() + facet_grid(Tp ~ .)
